d56d00194741cd97953a15e8fb337ae29bc38d59
Wendy Smoak <wsmoak@gmail.com>
2024-01-15 12:59:45 -0500

    Disable not-a-number filter and construct html for report

--- src/report_creator.py
+++ src/report_creator.py
@@ -10,18 +10,39 @@ class ReportCreator:
     def perform(account, model_name, date):
 
         entities_filename = f"{account}-{model_name}-entities-{date}.csv"
-        entities_df = pd.read_csv(entities_filename, sep=';')
+        entities_df = pd.read_csv(entities_filename, sep=';', na_filter=False)
         entities_df.sort_values('Label', inplace=True)
 
         attributes_filename = f"{account}-{model_name}-attributes-{date}.csv"
-        attributes_df = pd.read_csv(attributes_filename, sep=';')
+        attributes_df = pd.read_csv(attributes_filename, sep=';', na_filter=False)
         attributes_df.sort_values(['Entity label', 'Attribute label'], inplace=True)
 
+        lines = []
         for _index, row in entities_df.iterrows():
-            entity_label = row['Label']
-            description = row['Description']
-            print(f"Entity label: {entity_label}, Description: {description}")
-            filtered_attributes_df = attributes_df.loc[attributes_df['Entity label'] == entity_label]
-            for _index, row in filtered_attributes_df.iterrows():
-                print(f"     Entity label: {row['Entity label']}, Attribute label: {row['Attribute label']}")
+            entity_name = row['Label']
+            lines.append(f"<h1>{entity_name}</h1>")
+            lines.append(f"<p>{row['Description']}</p>")
+            lines.append("<hr>")
 
+            filtered_attributes_df = attributes_df.loc[attributes_df['Entity label'] == entity_name]
+            for _index, row in filtered_attributes_df.iterrows():
+                label = row['Attribute label']
+                format = row['Format']
+                description = row['Description']
+                notes = row['Notes']
+                table = row['Source table']
+                column = row['Source column']
+                constraints = row['Constraints']
+                lines.append("<p>")
+                lines.append(f"{label} ({format}) {description}")
+                if table and column:
+                    lines.append(f" | From {column} in {table}")
+                    if constraints:
+                        lines.append(f" as {constraints}")
+                    lines.append(".")
+                if notes:
+                    lines.append(f" | {notes}")
+                lines.append("</p>")
+
+
+        print(lines);
